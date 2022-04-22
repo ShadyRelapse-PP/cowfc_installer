a63153931a7b65343452e80260e9ba9fda4fb26b
ShadyRelapse-PP <74013844+ShadyRelapse-PP@users.noreply.github.com>
2022-04-22 22:37:06 +0100

    Update cowfc.sh

--- cowfc.sh
+++ cowfc.sh
@@ -68,7 +68,7 @@ mod1="proxy"      # This is a proxy mod that is dependent on the other 2
 mod2="proxy_http" # This is related to mod1
 mod3="php7.2"
 UPDATE_FILE="$0.tmp"
-UPDATE_BASE="https://raw.githubusercontent.com/EnergyCube/cowfc_installer/master/cowfc.sh"
+UPDATE_BASE="https://raw.githubusercontent.com/ShadyRelapse-PP/cowfc_installer/master/cowfc.sh"
 # Functions
 
 function update() {
